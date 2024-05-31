5fa366d40afd73a1a00aa047546730b469424048
bshongwe <dru.da.beat.hustle@gmail.com>
2024-05-31 08:42:02 +0200

    Update

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -17,39 +17,39 @@ const Projects = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
-          title="<project 1>"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          title="Blog Website"
+          des=" https://blog-website-drab-phi.vercel.app/
+              Blog website that uses Sanity, Tailwind and Next.js"
           src={projectOne}
         />
         <ProjectsCard
-          title="<project 2>"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          title="Brainwaive Landing Page"
+          des=" https://bshongwe.github.io/brainwave-ai-landing-page/
+              https://brainwave-ai-landing-page-cyw7i2eyv.vercel.app/"
           src={projectTwo}
         />
         <ProjectsCard
-          title="<project 3>"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          title="Earth_FX Trading App"
+          des=" https://earth-fx-trade-de9t40baq-ernest-bhekizwe-shongwes-projects.vercel.app/
+              Project still under development"
           src={projectThree}
         />
         <ProjectsCard
-          title="<project 4>"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          title="Nestec"
+          des=" https://bshongwe.github.io/Nestec_farm-app-landing-page/
+              Agriculture farming app, uses Bootstrap, Webpack, MongoDB + Redis cache-aside pattern"
           src={projectThree}
         />
         <ProjectsCard
-          title="<project 5>"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          title="ToDo List App"
+          des=" https://bshongwe.github.io/js-todo-list-app/
+              Pure HTML, JS & CSS"
           src={projectOne}
         />
         <ProjectsCard
-          title="Chatting App"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          title="eCommerce Website"
+          des=" https://online-shop-website-sage.vercel.app/
+              Project still under development"
           src={projectTwo}
         />
       </div>
