050ea3a973a4578801081cc1b52e9894ffa12284
bshongwe <dru.da.beat.hustle@gmail.com>
2024-08-07 12:34:28 +0200

    Animation adjustment for mobile devices: reducing word length

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -4,7 +4,7 @@ import Media from './Media';
 
 const LeftBanner = () => {
     const [text] = useTypewriter({
-      words: ["Full Stack Developer.", "Backend Developer.", "Frontend Developer.", "Mobile App Developer."],
+      words: ["Full Stack Dev.", "Backend Dev.", "Frontend Dev.", "Mobile App Dev."],
       loop: true,
       typeSpeed: 20,
       deleteSpeed: 10,
