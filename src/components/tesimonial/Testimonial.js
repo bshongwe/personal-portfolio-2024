56a7641861fbaa1ce82cc2eb54cc1c85b18f78df
bshongwe <dru.da.beat.hustle@gmail.com>
2024-08-07 18:56:29 +0200

    Update urls

--- src/components/tesimonial/Testimonial.js
+++ src/components/tesimonial/Testimonial.js
@@ -197,7 +197,7 @@ const Testimonial = () => {
               <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-8 rounded-lg shadow-shadowOne flex flex-col md:flex-row lgl:flex-col gap-8 justify-center md:justify-start lgl:justify-center">
                 <img
                   className="h-72 md:h-32 lgl:h-72 rounded-lg object-cover"
-                  src={testimonialOne}
+                  src={testimonialThree}
                   alt="Ernest Bhekizwe Shongwe"
                 />
                 <div className="w-full flex flex-col justify-end">
