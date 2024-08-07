d99af0a5c9efd5324a079ba49aea6e6676f8c470
bshongwe <dru.da.beat.hustle@gmail.com>
2024-08-07 17:32:36 +0200

    Blog link test: kept the original for reference

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -52,6 +52,10 @@ const Footer = () => {
             <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
               Blog
               <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
+              <a href="https://medium.com/@shongwe.bhekizwe" target="_blank" rel="noopener noreferrer" className="block">
+                Blog
+                <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
+              </a>
             </span>
           </li>
           <li>
