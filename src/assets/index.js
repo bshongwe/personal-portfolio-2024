d32d2fac7056bbb3bc427c32a64a92bddc10cd3b
bshongwe <dru.da.beat.hustle@gmail.com>
2024-08-08 13:12:31 +0200

    Update

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,9 +3,9 @@ import bannerImg from "./images/bannerImg.png";
 import projectOne from "./images/projects/projectOne.jpg";
 import projectTwo from "./images/projects/projectTwo.jpg";
 import projectThree from "./images/projects/projectThree.jpg";
-import testimonialOne from "./images/testmonial/testimonialA.png";
-import testimonialTwo from "./images/testmonial/testimonialB.png";
-import testimonialThree from "./images/testmonial/testimonialC.png";
+import testimonialOne from "./images/testmonial/testimonialOne.png";
+import testimonialTwo from "./images/testmonial/testimonialTwo.png";
+import testimonialThree from "./images/testmonial/testimonialThree.png";
 import quote from "./images/testmonial/quote.png";
 import contactImg from "./images/contact/contactImg.png";
 
