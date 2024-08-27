d00134af89eb511d7691056e173f2795815f9782
bshongwe <dru.da.beat.hustle@gmail.com>
2024-08-27 09:05:39 +0200

    Update new projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -18,39 +18,63 @@ const Projects = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
           title="Blog Website"
-          des=" https://blog-website-drab-phi.vercel.app/
-              Blog website that uses Sanity, Tailwind and Next.js"
+          des="Sanity | Tailwind | Next.js"
           src={projectOne}
         />
         <ProjectsCard
           title="Brainwaive Landing Page"
-          des=" https://bshongwe.github.io/brainwave-ai-landing-page/
-              https://brainwave-ai-landing-page-cyw7i2eyv.vercel.app/"
+          des="Will fill this in later"
           src={projectTwo}
         />
         <ProjectsCard
           title="Earth_FX Trading App"
-          des=" https://earth-fx-trade-de9t40baq-ernest-bhekizwe-shongwes-projects.vercel.app/
-              Project still under development"
+          des="Project still under development"
           src={projectThree}
         />
         <ProjectsCard
           title="Nestec"
-          des=" https://bshongwe.github.io/Nestec_farm-app-landing-page/
-              Agriculture farming app, uses Bootstrap, Webpack, MongoDB + Redis cache-aside pattern"
+          des="Agric farming app | Bootstrap | Webpack | MongoDB | Redis cache-aside pattern"
           src={projectThree}
         />
         <ProjectsCard
           title="ToDo List App"
-          des=" https://bshongwe.github.io/js-todo-list-app/
-              Pure HTML, JS & CSS"
+          des="HTML5, JS & CSS3"
           src={projectOne}
         />
         <ProjectsCard
-          title="eCommerce Website"
-          des=" https://online-shop-website-sage.vercel.app/
-              Project still under development"
+          title="NeXtro"
+          des="MERN | Tailwind | Docker | Firebase | Jest | JWT | Cloudinary"
+          src={projectThree}
+        />
+        <ProjectsCard
+          title="AI Pantry Tracker"
+          des="Next.js | React | Firebase | OpenAI | Clerk | Tailwind"
+          src={projectOne}
+        />
+        <ProjectsCard
+          title="AI Customer Support"
+          des="Next.js | React | Pinecone | OpenAI | Tailwind"
           src={projectTwo}
+        />
+        <ProjectsCard
+          title="AI Flashcards & Stripe"
+          des="Next.js | React | Firebase | OpenAI | Clerk | Stripe | Tailwind"
+          src={projectThree}
+        />
+        <ProjectsCard
+          title="Rate My Professor AI Assistant"
+          des="Next.js | React | Pinecone | OpenAI | Tailwind"
+          src={projectThree}
+        />
+        <ProjectsCard
+          title="Personal Website"
+          des="HTML5 | JS |SCSS | DNS"
+          src={projectOne}
+        />
+        <ProjectsCard
+          title="Recalla"
+          des="Next.js | React | Firebase | OpenAI | Clerk | Stripe | Tailwind"
+          src={projectThree}
         />
       </div>
     </section>
