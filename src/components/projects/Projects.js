5086f019cc14d0e3cc7edfeb24053214817017c4
bshongwe <dru.da.beat.hustle@gmail.com>
2024-09-27 13:09:18 +0200

    Update index and projects thumbnails

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Title from '../layouts/Title'
-import { projectOne, projectTwo, projectThree } from "../../assets/index";
+import { projectOne, projectTwo, projectThree, projectFour, projectFive } from "../../assets/index";
 import ProjectsCard from './ProjectsCard';
 
 const Projects = () => {
@@ -34,7 +34,7 @@ const Projects = () => {
         <ProjectsCard
           title="Nestec"
           des="Agric farming app | Bootstrap | Webpack | MongoDB | Redis (cache-aside pattern)"
-          src={projectThree}
+          src={projectFour}
         />
         <ProjectsCard
           title="ToDo List App"
@@ -49,32 +49,32 @@ const Projects = () => {
         <ProjectsCard
           title="AI Pantry Tracker"
           des="Next.js | React | Firebase | OpenAI | Clerk | Tailwind"
-          src={projectOne}
+          src={projectFour}
         />
         <ProjectsCard
           title="AI Customer Support"
           des="Next.js | React | Pinecone | OpenAI | Tailwind"
-          src={projectTwo}
+          src={projectFive}
         />
         <ProjectsCard
           title="AI Flashcards & Stripe"
           des="Next.js | React | Firebase | OpenAI | Clerk | Stripe | Tailwind"
-          src={projectThree}
+          src={projectFive}
         />
         <ProjectsCard
           title="Rate My Professor AI Assistant"
           des="Next.js | React | Pinecone | OpenAI | Tailwind"
-          src={projectThree}
+          src={projectFive}
         />
         <ProjectsCard
           title="Personal Website"
           des="HTML5 | JS |SCSS | DNS"
-          src={projectOne}
+          src={projectFive}
         />
         <ProjectsCard
           title="Recalla"
           des="Next.js | React | Firebase | OpenAI | Clerk | Stripe | Tailwind"
-          src={projectThree}
+          src={projectFive}
         />
       </div>
     </section>
