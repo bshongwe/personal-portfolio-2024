6475fcc1a8d0e79c453f4e24343153b652fe2be4
bshongwe <dru.da.beat.hustle@gmail.com>
2024-10-02 17:10:36 +0200

    Update: quick link features update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -46,7 +46,10 @@ const Footer = () => {
           </li>
           <li>
             <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
-              Services
+              <a href="#features" target="_blank" rel="noopener noreferrer" className="block">
+                Services
+                <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
+              </a>
               <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
             </span>
           </li>
