6f5c407db85b5f6c52975b8f44d793d1d7c68596
bshongwe <dru.da.beat.hustle@gmail.com>
2024-10-02 13:15:25 +0200

    Update links & icons

--- src/components/banner/Media.jsx
+++ src/components/banner/Media.jsx
@@ -1,31 +1,52 @@
 import React from 'react'
-import { FaFacebookF, FaTwitter, FaLinkedinIn, FaReact, FaPython } from "react-icons/fa";
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaReact, FaPython, FaGithub, FaGitlab, FaDiscord } from "react-icons/fa";
 import { SiTailwindcss, SiFigma, SiNextdotjs, SiTensorflow, SiDocker, SiKubernetes } from "react-icons/si";
 const Media = () => {
   return (
     <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
         <div>
           <h2 className="text-base uppercase font-titleFont mb-4">
-            SOCIALS
-          </h2>
-          <div className="flex gap-4">
-            <span className="bannerIcon">
-              <a href="https://www.facebook.com/shongwe.bhekizwe" target="_blank" rel="noopener noreferrer">
-                <FaFacebookF />
-              </a>
-            </span>
-            <span className="bannerIcon">
-              <a href="https://www.twitter.com/ernest_b_shong" target="_blank" rel="noopener noreferrer">
-                <FaTwitter />
-              </a>
-            </span>
-            <span className="bannerIcon">
-              <a href="https://www.linkedin.com/in/ernest-shongwe" target="_blank" rel="noopener noreferrer">
-                <FaLinkedinIn />
-              </a>
-            </span>
-          </div>
+          SOCIALS
+        </h2>
+        {/* First row of social icons */}
+        <div className="flex gap-4 mb-4">
+          <span className="bannerIcon">
+            <a href="https://www.facebook.com/shongwe.bhekizwe" target="_blank" rel="noopener noreferrer">
+              <FaFacebookF />
+            </a>
+          </span>
+          <span className="bannerIcon">
+            <a href="https://www.twitter.com/ernest_b_shong" target="_blank" rel="noopener noreferrer">
+              <FaTwitter />
+            </a>
+          </span>
+          <span className="bannerIcon">
+            <a href="https://www.linkedin.com/in/ernest-shongwe" target="_blank" rel="noopener noreferrer">
+              <FaLinkedinIn />
+            </a>
+          </span>
         </div>
+        {/* Second row of social icons */}
+        <div className="flex gap-4">
+          <span className="bannerIcon">
+            <a href="https://www.github.com/bshongwe" target="_blank" rel="noopener noreferrer">
+              <FaGithub />
+            </a>
+          </span>
+          <span className="bannerIcon">
+            <a href="https://www.gitlab.com/bshongwe" target="_blank" rel="noopener noreferrer">
+              <FaGitlab /> {/* GitLab icon */}
+            </a>
+          </span>
+          <span className="bannerIcon">
+            <a href="https://discord.com/invite/vqYpAVfk" target="_blank" rel="noopener noreferrer"> {/* Replace with your Discord link */}
+              <FaDiscord /> {/* Discord icon */}
+            </a>
+          </span>
+        </div>
+      </div>
+
+        {/* Best Skilled At Section */}
         <div>
           <h2 className="text-base uppercase font-titleFont mb-4">
             BEST SKILLED AT
