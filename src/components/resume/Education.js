d9afd3c159ae67f2145e4c192037c30736b9ca44
Ernest Bhekizwe Shongwe <126109055+bshongwe@users.noreply.github.com>
2024-12-31 20:28:35 +0200

    Update Education.js

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -12,7 +12,7 @@ const Education = () => {
       {/* part one */}
       <div>
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2009 - 2024</p>
+          <p className="text-sm text-designColor tracking-[4px]">2009 - 2025</p>
           <h2 className="text-3xl md:text-4xl font-bold">Education</h2>
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[Auto] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
@@ -20,67 +20,61 @@ const Education = () => {
             title="CyberOps Associate"
             subTitle="January 2025 - Present"
             result="Cisco Networking Academy"
-            des="SecOps | SOC | Cybersecurity | Forensics"
+            des="(Self Paced) SecOps | SOC | Cybersecurity | Forensics"
           />
           <ResumeCard
             title="ALX Applied Ai"
             subTitle="September 2024 - Present"
             result="ALX Africa"
-            des="ALX Fellowship Programme: Ai | RAG | Colab"
+            des="(Self Paced) ALX Fellowship Programme: Ai | RAG | Colab"
           />
           <ResumeCard
             title="Software Engineering Resident"
             subTitle="September 2024 - Present"
             result="Headstarter"
-            des="Artificial Intelligence Accelerator Programme"
+            des="(Instructor-led) Artificial Intelligence Accelerator Programme"
           />
           <ResumeCard
             title="Software Engineering"
             subTitle="July 2024 - Present"
             result="AWS educate | AWS Skillbuilder"
-            des="Self-paced certifications & badges"
+            des="(Self Paced) Certifications & Badges"
           />
           <ResumeCard
             title="Full Stack web Development certification"
             subTitle="January 2023 - Present"
             result="freeCodeCamp"
-            des="Full Stack | Backend | Frontend | Ai/ML"
+            des="(Self Paced) Full Stack | Backend | Frontend | Ai/ML"
           />
           <ResumeCard
             title="DevSecOps Learning Path"
             subTitle="September 2024 - Present"
             result="Google Cloud Skills Boost"
-            des="Hands-on Labs & Coursework Assessments"
+            des="(Self Paced) Hands-on Labs & Coursework Assessments"
           />
           <ResumeCard
             title="DevOps Training Course"
             subTitle="13 September 2024 - 09 December 2024"
             result="KalKey"
-            des="Linux, K8s and AWS: Terraform | Jenkins | Git | Docker | Ansible | Bash | CI/CD | IaC"
+            des="(Instructor-led) Linux, K8s, AWS & Azure: Terraform | Jenkins | Git | Azure | Docker | Ansible | Bash | CI/CD | IaaC"
           />
           <ResumeCard
             title="Software Engineering Fellowship"
             subTitle="July 2024 - September 2024"
             result="Headstarter"
-            des="Artificial Intelligence Fellowship"
+            des="(Self Paced) Artificial Intelligence Fellowship"
           />
           <ResumeCard
             title="Software Engineering - Full Stack (with Backend Specialization)"
-            subTitle="June 17, 2023 - Aprl 19, 2024 | April 22, 2023 - July 27, 2024"
+            subTitle="June 2023 - July 2024"
             result="ALX Africa"
-            des="certified"
-          />
-          <ResumeCard
-            title="Software Engineering"
-            subTitle="January 2023 - Aug 2023"
-            result="freeCodeCamp"
-            des="Self-paced certifications"
+            des="Certified on August 2024"
           />
           <ResumeCard
             title="Bachelor of Science Degree in Agronomy"
-            subTitle="2009 - 2012"
+            subTitle="August 2009 - May 2012"
             result="UNESWA"
-            des="graduated"
+            des="(Instructor-led) - graduated on October 2012"
           />
         </div>
       </div>
@@ -94,67 +88,67 @@ const Education = () => {
         <div className="mt-6 lgl:mt-14 w-full h-[Auto] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
             title="AWS Educate Web Builder"
-            subTitle="Dec. 2024"
+            subTitle="December 2024"
             result="AWS"
             des="AWS | Cloud | IaaC | Web Apps"
           />
           <ResumeCard
             title="DevOps Essentials"
-            subTitle="Dec. 2024"
+            subTitle="December 2024"
             result="Google"
             des="DevOps"
           />
           <ResumeCard
             title="AWS Educate Machine Learning - DeepRacer"
-            subTitle="Dec. 2024"
+            subTitle="December 2024"
             result="AWS"
             des="AWS | ML"
           />
           <ResumeCard
             title="AWS Educate Machine Learning Foundations"
-            subTitle="Dec. 2024"
+            subTitle="December 2024"
             result="AWS"
             des="AWS | ML"
           />
           <ResumeCard
             title="AWS Educate Getting Started with Security"
-            subTitle="Dec. 2024"
+            subTitle="December 2024"
             result="AWS"
             des="AWS | Cloud | DevSecOps"
           />
           <ResumeCard
             title="AWS Educate Getting Started with Serverless"
-            subTitle="Dec. 2024"
+            subTitle="December 2024"
             result="AWS"
             des="AWS | Cloud | Serverless Computing"
           />
           <ResumeCard
             title="AWS Educate Introduction to Generative Ai"
-            subTitle="Nov. 2024"
+            subTitle="November 2024"
             result="AWS"
             des="AWS | GenAi | cloud | Ai/ML"
           />
           <ResumeCard
             title="Agile Project Management"
-            subTitle="Dec. 2024"
+            subTitle="December 2024"
             result="HP Life | HP Foundation"
             des="Scrum | Kanban | Agile"
           />
           <ResumeCard
             title="AWS Educate Introduction to Cloud 101"
-            subTitle="Nov. 2024"
+            subTitle="November 2024"
             result="AWS"
             des="AWS"
           />
           <ResumeCard
             title="Developing a Google SRE Culture"
-            subTitle="Sept. 2024"
+            subTitle="September 2024"
             result="Google"
             des="AutoML | GCP"
           />
           <ResumeCard
             title="Google Developer Essentials"
-            subTitle="Sept. 2024"
+            subTitle="September 2024"
             result="Google"
             des="AutoML | GCP"
           />
