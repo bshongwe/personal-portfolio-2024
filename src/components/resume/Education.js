62f060db30d03911af29424db5e06ff010d426d0
Ernest Bhekizwe Shongwe <126109055+bshongwe@users.noreply.github.com>
2025-01-04 06:55:44 +0200

    Update Education.js

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -32,16 +32,16 @@ const Education = () => {
             title="Software Engineering Resident"
             subTitle="September 2024 - Present"
             result="Headstarter"
-            des="(Instructor-led) Artificial Intelligence Accelerator Programme"
+            des="(Instructor-led) Ai/ML Accelerator Programme"
           />
           <ResumeCard
-            title="Software Engineering"
+            title="Software Engineering: Cloud"
             subTitle="July 2024 - Present"
             result="AWS educate | AWS Skillbuilder"
-            des="(Self Paced) Certifications & Badges"
+            des="(Self Paced) Cloud Certifications & Badges"
           />
           <ResumeCard
-            title="Full Stack web Development certification"
+            title="Full Stack Web Development Certifications"
             subTitle="January 2023 - Present"
             result="freeCodeCamp"
             des="(Self Paced) Full Stack | Backend | Frontend | Ai/ML"
@@ -54,7 +54,7 @@ const Education = () => {
           />
           <ResumeCard
             title="DevOps Training Course"
-            subTitle="13 September 2024 - 09 December 2024"
+            subTitle="September 2024 - December 2024"
             result="KalKey"
             des="(Instructor-led) Linux, K8s, AWS & Azure: Terraform | Jenkins | Git | Azure | Docker | Ansible | Bash | CI/CD | IaaC"
           />
@@ -62,10 +62,10 @@ const Education = () => {
             title="Software Engineering Fellowship"
             subTitle="July 2024 - September 2024"
             result="Headstarter"
-            des="(Self Paced) Artificial Intelligence Fellowship"
+            des="(Self Paced) Ai/ML Fellowship Programme"
           />
           <ResumeCard
-            title="Software Engineering - Full Stack (with Backend Specialization)"
+            title="Software Engineering (with Backend Specialization)"
             subTitle="June 2023 - July 2024"
             result="ALX Africa"
             des="Certified on August 2024"
@@ -102,13 +102,13 @@ const Education = () => {
             title="AWS Educate Machine Learning - DeepRacer"
             subTitle="December 2024"
             result="AWS"
-            des="AWS | ML"
+            des="AWS | Ai/ML"
           />
           <ResumeCard
             title="AWS Educate Machine Learning Foundations"
             subTitle="December 2024"
             result="AWS"
-            des="AWS | ML"
+            des="AWS | Ai/ML"
           />
           <ResumeCard
             title="AWS Educate Getting Started with Security"
@@ -126,7 +126,7 @@ const Education = () => {
             title="AWS Educate Introduction to Generative Ai"
             subTitle="November 2024"
             result="AWS"
-            des="AWS | GenAi | cloud | Ai/ML"
+            des="AWS | GenAi | Cloud | Ai/ML"
           />
           <ResumeCard
             title="Agile Project Management"
@@ -138,7 +138,7 @@ const Education = () => {
             title="AWS Educate Introduction to Cloud 101"
             subTitle="November 2024"
             result="AWS"
-            des="AWS"
+            des="AWS | Cloud"
           />
           <ResumeCard
             title="Developing a Google SRE Culture"
@@ -160,51 +160,75 @@ const Education = () => {
           />
           <ResumeCard
             title="AWS Knowlegde: Serverless"
-            subTitle="2024"
+            subTitle="September 2024"
             result="AWS"
             des="AWS | API Gateway | Cloud | Lambda | Serverless Computing"
           />
           <ResumeCard
             title="AWS Knowlegde: Architecting"
-            subTitle="2024"
+            subTitle="September 2024"
             result="AWS"
             des="AWS | Cloud | Compute | AWS Core Services | AWS Database | Networking | Storage"
           />
           <ResumeCard
             title="AWS Knowlegde: AWS for Games: Cloud Game Development"
-            subTitle="2024"
+            subTitle="September 2024"
             result="AWS"
             des="AWS"
           />
           <ResumeCard
             title="ALX Software Engineering Programme"
-            subTitle="2024"
+            subTitle="August 2024"
             result="ALX Africa"
             des="Full-stack | Backend | CI/CD | REST APIs | JS | Python | C | TS | Unittesting | MySQL | NoSQL | SQL"
           />
           <ResumeCard
             title="Foundational C# with Microsoft Certification"
-            subTitle="2024"
+            subTitle="September 2023"
             result="freeCodeCamp | Microsoft Learn"
-            des="C# | Azure"
+            des="C# | Azure | Certification Exam: 92.5%"
+          />
+          <ResumeCard
+            title="Data Analysis with Python"
+            subTitle="September 2023"
+            result="freeCodeCamp"
+            des="Python | Ai/ML | Data Visualisation"
+          />
+          <ResumeCard
+            title="Scientific Computing with Python"
+            subTitle="September 2023"
+            result="freeCodeCamp"
+            des="Python | Ai/ML"
+          />
+          <ResumeCard
+            title="Diploma in DevOps Engineering - Kubernetes, Docker and Google Cloud"
+            subTitle="June 2023"
+            result="Alison | CPD"
+            des="DevOps: Microservices | VMs | K8s | GCP | Docker | WordPress | MySQL"
           />
           <ResumeCard
             title="Backend Development and APIs"
-            subTitle="2023"
+            subTitle="March 2023"
             result="freeCodeCamp"
-            des="JS | Replit | GitHub"
+            des="JS | Replit | GitHub | Microservices"
           />
           <ResumeCard
             title="Front End Development Libraries"
-            subTitle="2023"
+            subTitle="April 2023"
             result="freeCodeCamp"
             des="JS | Replit | GitHub | CodePen"
           />
           <ResumeCard
-            title="JavaScript Algorithms and Data Structures"
+            title="Legacy JavaScript Algorithms and Data Structures"
             subTitle="2023"
             result="freeCodeCamp"
             des="JS | Replit | GitHub"
+          />
+          <ResumeCard
+            title="Responsive Web Design"
+            subTitle="April 2023"
+            result="freeCodeCamp"
+            des="JS | Replit | GitHub | CodePen"
           />
         </div>
       </div>
