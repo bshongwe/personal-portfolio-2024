592afcbc68518141df4cb63011b665d972a901a2
Ernest Bhekizwe Shongwe <126109055+bshongwe@users.noreply.github.com>
2025-01-04 22:04:51 +0200

    Update App.js
    Removed commented out SEO block

--- src/App.js
+++ src/App.js
@@ -13,12 +13,6 @@ import Testimonial from "./components/tesimonial/Testimonial";
 function App() {
   return (
     <div className="w-full h-auto bg-bodyColor text-lightText px-4">
-        // {/* SEO Component */}
-        // <SEO 
-        //   title="Ernest B. Shongwe | Certified Software Engineer"
-        //   description="Explore my projects, testimonials and contact information."
-        //   keywords="Portfolio, Next.js, React, Developer, Software Engineer, Projects, Resume"
-        // />
         <Navbar />
       <div className="max-w-screen-xl mx-auto">
         <Banner />
