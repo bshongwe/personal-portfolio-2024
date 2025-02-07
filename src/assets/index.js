08ded2e3157534a7a1e6d71e911ae3559fb92544
Ernest Bhekizwe Shongwe <126109055+bshongwe@users.noreply.github.com>
2025-02-07 19:28:34 +0200

    Update index.js: Project 15 declaration correction

--- src/assets/index.js
+++ src/assets/index.js
@@ -14,7 +14,7 @@ import projectEleven from "./images/projects/projectEleven.jpg";
 import projectTwelve from "./images/projects/projectTwelve.jpg";
 import projectThirteen from "./images/projects/projectThirteen.jpg";
 import projectFourteen from "./images/projects/projectFourteen.jpg";
-import projectFourteen from "./images/projects/projectFifteen.jpg";
+import projectFifteen from "./images/projects/projectFifteen.jpg";
 import testimonialOne from "./images/testmonial/testimonialOne.png";
 import testimonialTwo from "./images/testmonial/testimonialTwo.png";
 import testimonialThree from "./images/testmonial/testimonialThree.png";
