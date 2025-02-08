05c346147e7d9a1a8facaf1991c873c644a6d4a5
Ernest Bhekizwe Shongwe <126109055+bshongwe@users.noreply.github.com>
2025-02-08 12:00:07 +0200

    Update index.js: Project 16

--- src/assets/index.js
+++ src/assets/index.js
@@ -15,6 +15,7 @@ import projectTwelve from "./images/projects/projectTwelve.jpg";
 import projectThirteen from "./images/projects/projectThirteen.jpg";
 import projectFourteen from "./images/projects/projectFourteen.jpg";
 import projectFifteen from "./images/projects/projectFifteen.jpg";
+import projectSixteen from "./images/projects/projectSixteen.jpg";
 import testimonialOne from "./images/testmonial/testimonialOne.png";
 import testimonialTwo from "./images/testmonial/testimonialTwo.png";
 import testimonialThree from "./images/testmonial/testimonialThree.png";
@@ -46,6 +47,7 @@ export {
   projectThirteen,
   projectFourteen,
   projectFifteen,
+  projectSixteen,
   testimonialOne,
   testimonialTwo,
   testimonialThree,
